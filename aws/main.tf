9ef1ce95356bec72ef9238dc4c0466b65675a781
jiangrui <deraiven@126.com>
2022-04-13 14:30:24 +0800

    aws provider demo

---
+++ aws/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAJTTSSUF2PB6HDCCA"
+  secret_key = "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"
+}
+
+variable "subnet_prefix" {
+  description = "cidr block for the subnet"
+
+}
+
+
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[0].cidr_block
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = var.subnet_prefix[0].name
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[1].cidr_block
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = var.subnet_prefix[1].name
+  }
+}
